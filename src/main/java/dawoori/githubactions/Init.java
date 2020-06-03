2c0e151cb1e2a013a212b57b382f822da6df1504
dawoori <daul@kookmin.ac.kr>
2020-06-03 16:27:45 +0900

    actions test - 1

--- src/main/java/dawoori/githubactions/Init.java
+++ src/main/java/dawoori/githubactions/Init.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Component;
 public class Init implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("h2");
+        System.out.println("h2.");
     }
 }
